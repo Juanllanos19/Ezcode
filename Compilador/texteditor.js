42a0130ea209b4c4696f6e510b60e5e5896f3514
HaikerMaiker <haikermaiker@gmail.com>
2023-04-23 19:25:42 -0600

    SP2T1 test cases y compilador v2

--- Compilador/texteditor.js
+++ Compilador/texteditor.js
@@ -3,18 +3,43 @@ const CPP_KEY = "53";
 const PYTHON_KEY = "70";
 const BASE_URL = "https://judge0-ce.p.rapidapi.com/submissions";
 
+var exercises;
+var title;
+var desc;
+
+
+fetch('./testcases.json')
+    .then(res => res.json())
+    .then(data => importJson(data))
+
+
+function importJson(info)
+{
+    exercises = info;
+
+    title = exercises[0].title;
+    desc = exercises[0].description;
+    $("#nombre").html(title);
+    $("#descripcion").html(desc);
+    console.log(exercises);
+}
+
 
 function codeEditor(lang_id) {
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/twilight");
 
-  console.log("id" + lang_id )
+  var editor2 = ace.edit("editor2");
+  editor2.setTheme("ace/theme/twilight");
+
+  console.log("id:" + lang_id )
   $(document).ready(function () {
     $("button").click(function () {
       let code = editor.getValue();
+      let input = editor2.getValue();
       $("#ans").html("Loading...");
       console.log(code);
-      let data = {
+      /*let data = {
         source_code: code,
         language_id: lang_id,
         number_of_runs: "1",
@@ -29,16 +54,17 @@ function codeEditor(lang_id) {
         enable_per_process_and_thread_time_limit: false,
         enable_per_process_and_thread_memory_limit: false,
         max_file_size: "1024",
-      };
+      };*/
       const options = {
         method: 'POST',
         headers: {
           'content-type': 'application/json',
           'Content-Type': 'application/json',
-          'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+          //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+          'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
           'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
         },
-        body: '{"language_id":92,"source_code":"' + btoa(code) +'=","stdin":"SnVkZ2Uw"}'
+        body: '{"language_id":92,"source_code":"' + btoa(code) +'=","stdin":"' + btoa(input) + '", "redirect_stderr_to_stdout": true}'
       };
 
       
@@ -51,8 +77,8 @@ function codeEditor(lang_id) {
       const options2 = {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
-          //'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+          //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+          'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
           'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
         }
       };
@@ -80,56 +106,112 @@ function codeEditor(lang_id) {
       }
       let request = retrieve();
 
+
+    });
+    $("button2").click(function () {
+      console.log("Boton2 activado");
+      let code = editor.getValue();
+      $("#ans").html("Loading...");
+      let problemid = 0;
+
+      var outputs = "---Tests---\n";
+
+      var options = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'Content-Type': 'application/json',
+          //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+          'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+          'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        },
+        body: '{"language_id":92,"source_code":"' + btoa(code) +'=","stdin":"' + '' + '", "redirect_stderr_to_stdout": true}'
+      };
+
       
-      /*let request = $.ajax(
+      async function retrieve(){fetch('https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&fields=*', options)
+        .then(response => response.json())
+        .then(response => doubledown(response.token))
+        //.then(response => console.log(response.token))
+        .catch(err => console.error(err));
+      }
+      const options2 = {
+        method: 'GET',
+        headers: {
+          //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+          'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+          'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        }
+      };
+      
+      async function doubledown(key){
+        console.log(key);
+        //console.log(key.token);
+        fetch('https://judge0-ce.p.rapidapi.com/submissions/' + key + '?base64_encoded=true&fields=*', options2)
+        .then(response => response.json())
+        .then(response => changeOutput(response))
+        .catch(err => console.error(err));
+      }
+
+      async function changeOutput(texto){
+        if (texto.finished_at === null)
         {
-        url: BASE_URL,
-        type: "post",
-        data: data,
-      });*/
-      /*
-      const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-      // Callback handler that will be called on success
-      request.done(async function (response, textStatus, jqXHR) {
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-        let token = response.token;
-        await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 sec
-        console.log(3, "after 3 seconds");
-        let second_request = $.ajax({
-          url: BASE_URL + "/"+ token,
-          type: "get",
-        });
-        second_request.done(function (response) {
-          console.log(response.stdout);
-          $("#ans").html(response.stdout);
-        });
-      });
-      */
+          await doudbledown(texto.token);
+        }
+        else{
+          let inp = texto.stdin;
+          let out = texto.stdout;
+          let expectedOut = texto.expected_output;
+
+          outputs = outputs + "Input: " + atob(inp) + "\n";
+          outputs = outputs + "Output: " + atob(out) + "\n";
+          outputs = outputs + "Expected Output: " + atob(expectedOut) + "\n";
+          if (out == expectedOut)
+          {
+            outputs = outputs + "Result: SUCCESS" + "\n\n";
+          }
+          else
+          {
+            outputs = outputs + "Result: FAILED" + "\n\n";
+          }
+          console.log(outputs);
+          $("#ans").html(outputs);
+        }
+      }
+
+      async function test(num){
+        options = {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'Content-Type': 'application/json',
+            //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+            'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+          },
+          body: '{"language_id":92,"source_code":"' + btoa(code) +'=","stdin":"' + btoa(exercises[problemid].tests[num].input) + '", "redirect_stderr_to_stdout": true, "expected_output": "' + btoa(exercises[problemid].tests[num].output) +'"}'
+        };
+
+        await retrieve();
+      }
+
+      async function runTests()
+      {
+        for (let i = 0; i < exercises[problemid].tests.length; i++)
+        {
+          console.log(exercises[problemid].tests[i].input);
+          //outputs = outputs + "Test " + exercises[problemid].tests[i].testId + "\n";
+          await test(i);
+        }
+      }
+      runTests();
+      //test(0);
+      
+
     });
   });
-  if(lang_id==PYTHON_KEY)
-      editor.setValue("def execute(): \n\t for i in range(10):\n\t\t print i \nexecute()")
-  //java
-  if(lang_id==JAVA_KEY){
-
-      let javacode = `public class Main{
-  public static void main(String args[]){
-    System.out.println("hello");
-  }
-}
-`;
-
-  editor.setValue(javacode)
-
-  }if(lang_id==CPP_KEY){
-      let cppcode = `#include <iostream>
-using namespace std;
-  int main() {
-      cout<<"Hello World"; \n
-}`
-      editor.setValue(cppcode)
-  }
+  editor.setValue("def execute(): \n\t for i in range(10):\n\t\t print(i) \nexecute()");
+  editor2.setValue("#Your inputs here");
 
 
 } 
