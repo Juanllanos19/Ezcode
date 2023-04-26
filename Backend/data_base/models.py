d39519443b830c8c5f17d431568dcb5bbfcee9b6
Juan Carlos <o.llanos.juan19@gmail.com>
2023-04-25 18:03:52 -0600

    SP2T3 Se actualiza el models

--- Backend/data_base/models.py
+++ Backend/data_base/models.py
@@ -8,10 +8,10 @@ class Departamento(models.Model):
 
 class Profesor(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellidoPat = models.CharField(max_length=50)
-    apellidoMat = models.CharField(max_length=50)
-    especialidad = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=250)
+    apellidoPat = models.CharField(max_length=250)
+    apellidoMat = models.CharField(max_length=250)
+    especialidad = models.CharField(max_length=250)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     correo = models.EmailField()
     contrasena = models.CharField('Contraseña', max_length=500)
@@ -25,33 +25,32 @@ class Periodo(models.Model):
         return self.fechaInicio + ' - ' + self.fechaFin
 
 class Tema(models.Model):
-    tipo: models.TextField()
+    nombre = models.CharField(max_length=250)
+    tipo = models.TextField()
     def _str_(self) :
-        return self.tipo
+        return self.nombre
+    
+class Dificultad(models.Model):
+    rango = models.CharField(max_length=20)
+    def _str_(self) :
+        return self.rango
     
 class Pregunta(models.Model):
     titulo = models.CharField(max_length=500)
     tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
     contenido = models.FileField(upload_to = 'data_base/JSONdoc/')
+    tipoP = models.BooleanField("tipo de pregunta | Abierta 0 | Cerrada 1")
+    estado = models.BooleanField(default=0)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    dificultad = models.ForeignKey(Dificultad, on_delete=models.CASCADE)
     def _str_(self) :
         return self.titulo
-
-class Admin(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellidoPat = models.CharField(max_length=50)
-    apellidoMat = models.CharField(max_length=50)
-    correo = models.EmailField()
-    contrasena = models.CharField('Contraseña', max_length=500)
-    def _str_(self) :
-        return self.nombre + ' ' + self.apellidoPat + ' ' + self.apellidoMat
     
 class Actividad(models.Model):
     nombre = models.CharField(max_length=500)
     fechaInicio = models.DateField('Fecha de inicio')
     fechaFin = models.DateField('Fecha de fin')
     duracion = models.DurationField()
-    pregunta = models.ManyToManyField(Pregunta)
     def _str_(self) :
         return self.nombre
 
@@ -68,7 +67,6 @@ class Grupo(models.Model):
     estado = models.BooleanField()
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
     periodo = models.ForeignKey(Periodo, on_delete=models.CASCADE)
-    actividad = models.ManyToManyField(Actividad)
     def _str_(self) :
         return self.nombre + '.' + self.uf
     
@@ -87,14 +85,32 @@ class Estudiante(models.Model):
     matricula = models.CharField(max_length=9)
     correo = models.EmailField()
     contrasena = models.CharField('Contraseña', max_length=500)
-    calificacion = models.ManyToManyField(Actividad, through='Calificacion')
     def _str_(self) :
         return self.matricula + ': ' + self.nombre + ' ' + self.apellidoPat
 
+class EstudianteGrupo(models.Model):
+    estudiante = models.ForeignKey(Estudiante,on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo,on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return super().__str__()
+
+class ActividadGrupo(models.Model):
+    grupo =models.ForeignKey(Grupo,on_delete=models.CASCADE)
+    actividad =models.ForeignKey(Actividad,on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return super().__str__()
+   
+class ActividadPregunta(models.Model):
+    actividad = models.ForeignKey(Actividad,on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta,on_delete=models.CASCADE)
+    valor = models.IntegerField(default=1)
+    def __str__(self) -> str:
+        return super().__str__()
+
 class Calificacion(models.Model):
-    actividades = models.ForeignKey(Actividad, on_delete=models.CASCADE)
-    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name='calificaciones')
-    calificacion = models.IntegerField(default=0)
-    comentario = models.TextField()
+    ponderacion = models.IntegerField(default=0)
+    puntosTotal = models.IntegerField("Puntos totales", default=0)
+    actividad = models.ForeignKey(ActividadPregunta,on_delete=models.CASCADE)
+    estudiante = models.ForeignKey(Estudiante,on_delete=models.CASCADE)
     def _str_(self) :
-        return str(self.calificacion) + ' ' + self.comentario
+        return self.ponderacion + '/' + self.puntosTotal
