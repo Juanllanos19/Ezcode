ffb126f472e8b641eaa6304912ceead11f659543
Juan Carlos <o.llanos.juan19@gmail.com>
2023-04-25 18:05:56 -0600

    SP2T3 Se actualiza el admin

--- Backend/data_base/admin.py
+++ Backend/data_base/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Departamento, Profesor, Periodo, Tema, Pregunta, Admin, Actividad, Grupo, Carrera, Estudiante, Calificacion, Uf
+from .models import Departamento, Profesor, Periodo, Tema, Pregunta, Dificultad, Actividad, Grupo, Carrera, Estudiante, Calificacion, Uf, EstudianteGrupo, ActividadGrupo, ActividadPregunta
 # Register your models here.
 admin.site.register(Departamento)
 admin.site.register(Profesor)
 admin.site.register(Periodo)
 admin.site.register(Tema)
 admin.site.register(Pregunta)
-admin.site.register(Admin)
+admin.site.register(Dificultad)
 admin.site.register(Actividad)
 admin.site.register(Grupo)
 admin.site.register(Carrera)
 admin.site.register(Estudiante)
 admin.site.register(Calificacion)
 admin.site.register(Uf)
+admin.site.register(EstudianteGrupo)
+admin.site.register(ActividadGrupo)
+admin.site.register(ActividadPregunta)
