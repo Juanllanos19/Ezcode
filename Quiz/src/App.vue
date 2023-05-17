d6c48a36c031765600bc397ff688e4383a169ba6
Santiago <a01734958@tec.mx>
2023-05-17 16:30:28 -0600

    SP3T9 navbar responsiva pero se ve un poquis fea

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -4,13 +4,27 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
 
+<nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+  <div class="container">
+    <a class="navbar-brand logo-text" href="#"><img id="logo-navbar" src="./assets/A01732454_Poster.png" alt=""></a> 
+    <button style="background-color: gainsboro;" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <div class="navbar-nav">
+              <RouterLink to="/" class="nav-link text-white">PRODUCTO</RouterLink>
+              <RouterLink to="/" class="nav-link text-white">NOSOTROS</RouterLink>
+              <RouterLink to="/" id="sign-up-navbar" class="nav-link text-white">SIGN UP</RouterLink>
+            </div>
+    </div>
+  </div>
+</nav>
 
 
-
-  <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+  <!-- <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
     <div class="container">
         <a class="navbar-brand logo-text" href="#"><img id="logo-navbar" src="./assets/A01732454_Poster.png" alt=""></a> 
-        <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
@@ -21,7 +35,7 @@ import { RouterLink, RouterView } from 'vue-router'
             </div>
         </div> 
     </div> 
-  </nav>
+  </nav> -->
   <RouterView />
 </template>
 
