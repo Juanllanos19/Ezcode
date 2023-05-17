ed314d3e24fe80e558e96ca2beedf73cab09bd2f
Santiago <a01734958@tec.mx>
2023-05-17 16:06:41 -0600

    SP3T9 navbar casi lista

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -3,96 +3,39 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar w/ text</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-          <span class="navbar-text">
-            Navbar text with an inline element
-          </span>
-        </div>
-      </div>
-    </nav>
-  </header>
 
+
+
+
+  <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+    <div class="container">
+        <a class="navbar-brand logo-text" href="#"><img id="logo-navbar" src="./assets/A01732454_Poster.png" alt=""></a> 
+        <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+          <div class="navbar-nav">
+              <RouterLink to="/" class="nav-link text-white">PRODUCTO</RouterLink>
+              <RouterLink to="/" class="nav-link text-white">NOSOTROS</RouterLink>
+              <RouterLink to="/" id="sign-up-navbar" class="nav-link text-white">SIGN UP</RouterLink>
+            </div>
+        </div> 
+    </div> 
+  </nav>
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+#navbarExample{
+  backdrop-filter: blur(10px);
+  height: 15%;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#logo-navbar {
+  width: 20%;
 }
-
-nav {
+.nav-link{
+  letter-spacing: 7px;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  white-space: nowrap;
 }
 </style>
--- Quiz/src/views/HomeView.vue
+++ Quiz/src/views/HomeView.vue
@@ -1,7 +1,112 @@
-<script setup></script>
+<script setup>
+import { loadFull } from "tsparticles";
+
+const particlesInit = async engine => {
+    await loadFull(engine);
+};
+
+const particlesLoaded = async container => {
+    console.log("Particles container loaded", container);
+};
+</script>
 
 <template>
-  <main>
-    <h1>Inicio</h1>
-  </main>
+  <div id="app">
+      <Particles
+          id="tsparticles"
+          :particlesInit="particlesInit"
+          :particlesLoaded="particlesLoaded"
+          url="http://foo.bar/particles.json"
+      />
+
+      <Particles
+          id="tsparticles"
+          :particlesInit="particlesInit"
+          :particlesLoaded="particlesLoaded"
+          :options="{
+                  background: {
+                      color: {
+                          value: '#181848'
+                      }
+                  },
+                  fullScreen:{
+                    enable: true,
+                    zIndex: -1,
+                  },
+                  fpsLimit: 120,
+                  interactivity: {
+                      events: {
+                          onClick: {
+                              enable: true,
+                              mode: 'push'
+                          },
+                          onHover: {
+                              enable: true,
+                              mode: 'repulse'
+                          },
+                          resize: true
+                      },
+                      modes: {
+                          bubble: {
+                              distance: 400,
+                              duration: 2,
+                              opacity: 0.8,
+                              size: 40
+                          },
+                          push: {
+                              quantity: 4
+                          },
+                          repulse: {
+                              distance: 200,
+                              duration: 0.4
+                          }
+                      }
+                  },
+                  particles: {
+                      color: {
+                          value: '#ffffff'
+                          // value: '#4654A6'
+                      },
+                      links: {
+                          color: '#ffffff',
+                          // color: '#4EA641',
+                          distance: 150,
+                          enable: true,
+                          opacity: 0.5,
+                          width: 1
+                      },
+                      collisions: {
+                          enable: true
+                      },
+                      move: {
+                          direction: 'none',
+                          enable: true,
+                          outMode: 'bounce',
+                          random: false,
+                          speed: 1,
+                          straight: false
+                      },
+                      number: {
+                          density: {
+                              enable: true,
+                              area: 800
+                          },
+                          value: 100
+                      },
+                      opacity: {
+                          value: 0.5
+                      },
+                      shape: {
+                          type: 'circle'
+                      },
+                      size: {
+                          random: true,
+                          value: 5
+                      }
+                  },
+                  detectRetina: true
+              }"
+      />
+  </div>
 </template>
+
