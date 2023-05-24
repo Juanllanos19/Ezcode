15f175cf43de189b3268dcdf3cd91162043cd38e
Santiago León <85116254+Sant1001@users.noreply.github.com>
2023-05-24 17:42:42 -0600

    SP4T9 separe background como componente

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -2,20 +2,10 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 </script>
-<script>
-import Nav from './components/Nav.vue'
-
-export default {
-  components: {
-    Nav
-  }
-}
-</script>
 
 <template>
   <div id="app">
     <header>
-      <!-- <Nav /> -->
     </header>
     <RouterView />
   </div>
