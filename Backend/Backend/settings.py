af86587c0505994db416f22a6981cf383544f0ee
Juan Carlos Llanos <o.llanos.juan19@gmail.com>
2023-05-24 20:10:04 -0600

    ya quedó el back

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -106,16 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.UppercasePasswordValidator',
-    },
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.UppercasePasswordValidator',
+    #},
 ]
 
-PASSWORD_HASHERS = [
-    'passlib.hash.pbkdf2_sha256',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-
-]
+#PASSWORD_HASHERS = [
+#    'passlib.hash.pbkdf2_sha256',
+#    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+#
+#]
 
 
 # Internationalization
