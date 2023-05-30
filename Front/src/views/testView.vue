754d1b04d744496e40c4b94060b4aa79e1ebc074
HaikerMaiker <haikermaiker@gmail.com>
2023-05-29 22:16:18 -0600

    Iker: Remake del compilador ya funcional

--- Front/src/views/testView.vue
+++ Front/src/views/testView.vue
@@ -16,7 +16,7 @@ const data = ref([
             contenido:{}
         }
     ]);
-const contenido = ref([{}])
+const contenido = ref([{}]);
     onMounted(
         axios.get('http://localhost:8000/api/pregunta/1/')
         .then((result) => {
@@ -27,7 +27,7 @@ const contenido = ref([{}])
             axios.get(data.value.contenido)
             .then((response) => {
                 contenido.value = response.data;
-                console.log(contenido.description)
+                console.log(contenido.description);
             }
             ).catch((error) => {
                     console.log(error)
@@ -53,13 +53,17 @@ data() {
         value: '#Your inputs here!'
       },
       output: {
-        value: 'Output'
+        value: ''
+      },
+      testcases: {
+        value: [],
+        results: []
       },
     }
   },
   methods:{
     execute(event){
-        console.log("Execute")
+        console.log("Execute");
 
         const options = {
         method: 'POST',
@@ -71,7 +75,8 @@ data() {
         headers: {
             'content-type': 'application/json',
             'Content-Type': 'application/json',
-            'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+            //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+            'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
             'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
         },
         data: {
@@ -90,7 +95,8 @@ data() {
                 fields: '*'
             },
             headers: {
-                'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+                //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+                'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
                 'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
             }
         };
@@ -118,7 +124,7 @@ data() {
                 console.log(response.data);
                 if (response.data.stdout == null)
                 {
-                    //getData2();
+                    getData2();
                 }
                 else
                 {
@@ -132,7 +138,91 @@ data() {
     },
     test(event){
         console.log("Testing")
-        console.log(btoa(this.editor.value))
+        this.testcases.value = [];
+        this.testcases.results = [];
+        const options = {
+        method: 'POST',
+        url: 'https://judge0-ce.p.rapidapi.com/submissions',
+        params: {
+            base64_encoded: 'true',
+            fields: '*'
+        },
+        headers: {
+            'content-type': 'application/json',
+            'Content-Type': 'application/json',
+            //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+            'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        },
+        data: {
+            language_id: 92,
+            source_code: btoa(this.editor.value),
+            stdin: btoa(this.input_editor.value),
+            redirect_stderr_to_stdout: true,
+        }
+        };
+
+        const options2 = {
+            method: 'GET',
+            url: 'https://judge0-ce.p.rapidapi.com/submissions/2e979232-92fd-4012-97cf-3e9177257d10',
+            params: {
+                base64_encoded: 'true',
+                fields: '*'
+            },
+            headers: {
+                //'X-RapidAPI-Key': '0ae7a38b45msh71c6a1fcbf098e2p1d5efbjsn3ccb076e28db',
+                'X-RapidAPI-Key': '9e626646d1mshd9fd0c9fed686ecp1bbed0jsn8815f340f24a',
+                'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+            }
+        };
+        
+        const getData = async (index) => {
+            try {
+                const response = await axios.request(options);
+                console.log(response.data);
+                options2.url= 'https://judge0-ce.p.rapidapi.com/submissions/' + response.data.token;
+                getData2(index);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        const getData2 = async (index) => {
+            try {
+                const response = await axios.request(options2);
+                console.log(response.data);
+                if (response.data.stdout == null)
+                {
+                    getData2(index);
+                }
+                else
+                {
+                    var salida = atob(response.data.stdout);
+                    this.testcases.value[index] = "Output: " + salida;
+                    if (salida == atob(response.data.expected_output))
+                    {
+                        this.testcases.results[index] = "checkmark";
+                    }
+                    else{
+                        this.testcases.results[index] = "tacha";
+                    }
+
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        for (var i = 0; i < contenido.value.tests.length; i++)
+        {
+            options.data.stdin = btoa(contenido.value.tests[i].input);
+            options.data.expected_output = btoa(contenido.value.tests[i].output);
+            this.testcases.results.push(null);
+            this.testcases.value.push(null);
+            getData(i);
+            console.log(options.data.source_code );
+            console.log(atob(options.data.source_code));
+        }
     }
   }
 }
@@ -185,6 +275,31 @@ data() {
 
                               <h5 class="card-title" style="padding-top: 5%;">Output</h5>
                               {{ output.value }}
+                              <h5 class="card-title" style="padding-top: 5%;">Test cases</h5>
+
+                            
+                              <div class="accordion" id="accordionExample">
+                                <template v-for="(item, index) in contenido.tests" :key="index">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="'#collapse'+index">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse'+index" aria-expanded="false" :aria-controls="'collapse'+index">
+                                                Test: {{ item.testId }}
+                                                <img v-if="testcases.results[index] != null" :src="'../src/assets/' + testcases.results[index]+'.png'" style="max-width: 8%; margin-left: 75%;">
+                                            </button>
+                                        </h2>
+                                        <div :id="'collapse'+index" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                Input: {{ item.input }}
+                                                <br>
+                                                Expected output: {{ item.output }}
+                                                <br>
+                                                {{ testcases.value[index] }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </template>
+                               
+                            </div>
                         </div>
                     </div>
                 </div>
