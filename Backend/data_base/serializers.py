1eec64d3d0ac94779b49302d5a56a26e21f3e53b
Juan Carlos Llanos <o.llanos.juan19@gmail.com>
2023-05-30 11:24:32 -0600

    se modifica los serializers

--- Backend/data_base/serializers.py
+++ Backend/data_base/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework.serializers import ModelSerializer, ALL_FIELDS
 from .models import Departamento, Profesor, Periodo, Tema, Dificultad, Pregunta, Actividad, Uf, Grupo, Carrera, Estudiante, EstudianteGrupo, ActividadGrupo, ActividadPregunta, Calificacion
-#commit de prueba
 class DepartamentoSerializer(ModelSerializer):
     class Meta:
         model = Departamento
         fields = ALL_FIELDS
 
 class ProfesorSerializer(ModelSerializer):
+    class Meta:
+        model = Profesor
+        fields = ALL_FIELDS
+
+class ProfesorGetSerializer(ModelSerializer):
     departamento = DepartamentoSerializer()
     class Meta:
         model = Profesor
@@ -28,6 +32,11 @@ class DificultadSerializer(ModelSerializer):
         fields = ALL_FIELDS
 
 class PreguntaSerializer(ModelSerializer):
+    class Meta:
+        model = Pregunta
+        fields = ALL_FIELDS
+
+class PreguntaGetSerializer(ModelSerializer):
     tema = TemaSerializer()
     profesor =ProfesorSerializer()
     dificultad = DificultadSerializer()
@@ -45,7 +54,13 @@ class UfSerializer(ModelSerializer):
         model = Uf
         fields = ALL_FIELDS
 
+
 class GrupoSerializer(ModelSerializer):
+    class Meta:
+        model = Grupo
+        fields = ALL_FIELDS
+
+class GrupoGetSerializer(ModelSerializer):
     uf = UfSerializer()
     profesor = ProfesorSerializer()
     periodo = PeriodoSerializer()
@@ -59,12 +74,23 @@ class CarreraSerializer(ModelSerializer):
         fields = ALL_FIELDS
 
 class EstudianteSerializer(ModelSerializer):
+    class Meta:
+        model = Estudiante
+        fields = ALL_FIELDS
+
+class EstudianteGetSerializer(ModelSerializer):
     carrera = CarreraSerializer()
     class Meta:
         model = Estudiante
         fields = ALL_FIELDS
 
 class EstudianteGrupoSerializer(ModelSerializer):
+    class Meta:
+        model = EstudianteGrupo
+        fields = ALL_FIELDS
+
+
+class EstudianteGrupoGetSerializer(ModelSerializer):
     estudiante = EstudianteSerializer()
     grupo = GrupoSerializer()
     class Meta:
@@ -72,6 +98,12 @@ class EstudianteGrupoSerializer(ModelSerializer):
         fields = ALL_FIELDS
 
 class ActividadGrupoSerializer(ModelSerializer):
+    class Meta:
+        model = ActividadGrupo
+        fields = ALL_FIELDS
+
+
+class ActividadGrupoGetSerializer(ModelSerializer):
     grupo = GrupoSerializer()
     actividad = ActividadSerializer()
     class Meta:
@@ -79,6 +111,12 @@ class ActividadGrupoSerializer(ModelSerializer):
         fields = ALL_FIELDS
 
 class ActividadPreguntaSerializer(ModelSerializer):
+    class Meta:
+        model = ActividadPregunta
+        fields = ALL_FIELDS
+
+
+class ActividadPreguntaGetSerializer(ModelSerializer):
     actividad = ActividadSerializer()
     pregunta = PreguntaSerializer()
     class Meta:
@@ -86,6 +124,11 @@ class ActividadPreguntaSerializer(ModelSerializer):
         fields = ALL_FIELDS
 
 class CalificacionSerializer(ModelSerializer):
+    class Meta:
+        model = Calificacion
+        fields = ALL_FIELDS
+
+class CalificacionGetSerializer(ModelSerializer):
     actividad = ActividadPreguntaSerializer()
     estudiante = EstudianteSerializer()
     class Meta:
