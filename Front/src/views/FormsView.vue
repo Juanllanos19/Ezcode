e8de4b44d3729a7a105fbe8b4f4e1d5d1b7c65f3
JosuAlejandroAl <A01732454@itesm.mx>
2023-06-01 14:39:09 -0600

    cambios de los componentes de la pantalla de forms del profesor

--- Front/src/views/FormsView.vue
+++ Front/src/views/FormsView.vue
@@ -5,18 +5,12 @@
                 <h6 class="card-title mb-3">Seleccione el tipo de pregunta</h6>
                 <div class="d-flex flex-row">
                     <label class="radio mr-1">
-                        <input type="radio" name="add" value="anz" v-model="questionType.value"
-                            @change="handleQuestionTypeChange('codigo')" checked>
-                        <span>
-                            <i class="fa fa-user"></i> Codigo
-                        </span>
+                        <input type="radio" name="questionType" value="codigo" v-model="questionType">
+                        <span><i class="fa fa-user"></i> Codigo</span>
                     </label>
                     <label class="radio">
-                        <input type="radio" name="add" value="add" v-model="questionType.value"
-                            @change="handleQuestionTypeChange('multiple')" checked>
-                        <span>
-                            <i class="fa fa-plus-circle"></i> Multiple
-                        </span>
+                        <input type="radio" name="questionType" value="multiple" v-model="questionType">
+                        <span><i class="fa fa-plus-circle"></i> Multiple</span>
                     </label>
                 </div>
                 <h6 class="information mt-4">Personalice la pregunta</h6>
@@ -39,28 +33,19 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="form-group">
-                                <!-- <label for="name">Name</label> --> <input class="form-control" type="text"
-                                    placeholder="Descripción">
+                                <div class="input-group"> <input class="form-control" type="text" placeholder="Driver">
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <textarea class="form-control" aria-label="With textarea" placeholder="Descripción"></textarea>
                         </div>
                     </div>
 
                     <!-- Filtro seccion -->
                     <div class="d-flex flex-row">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <input type="number" id="replyNumber" min="0" data-bind="value:replyNumber"
-                                        placeholder="Ponderacion" required v-model="ponderation">
-                                </div>
-                            </div>
-                        </div> &nbsp;&nbsp;
-                        <div class="content-wrapper">
-                            <label for="datepicker">Fecha y hora</label>
-                            <datepicker v-model="selectedDate" format="yyyy-MM-dd HH:mm" :clear-button="true"
-                                :editable="false">
-                            </datepicker>
-                        </div> &nbsp;&nbsp;
                         <label for="referrer"> ¿Cual es la dificultad?
                             <select id="inputState" class="form-select" v-model="selectedDifficulty">
                                 <option selected>Choose...</option>
@@ -83,27 +68,21 @@
                     </div>
                     <div class="d-flex flex-row">
                         <div class="row">
-                            <div v-for="(boxOutCode, index) in boxOutCode" :key="index" class="col-12">
-                                <a for="inputAddress" class="form-label">Output {{ index + 1 }}</a>
-                                <input type="text" class="form-control" v-model="boxText">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Input">
+                                <input type="text" class="form-control" placeholder="Output">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="button" class="btn btn-primary" @click="addOutCode">AñadirOut</button>
-                                <button type="button" class="btn btn-danger" @click="deleteOutCode">EliminarOut</button>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Input">
+                                <input type="text" class="form-control" placeholder="Output">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div v-for="(boxIntCode, index) in boxIntCode" :key="index" class="col-12">
-                                <a for="inputAddress" class="form-label"> Input {{ index + 1 }}</a>
-                                <input type="text" class="form-control" v-model="boxText">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Input">
+                                <input type="text" class="form-control" placeholder="Output">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="button" class="btn btn-primary" @click="addIntCode">AñadirInt</button>
-                                <button type="button" class="btn btn-danger" @click="deleteIntCode">EliminarInt</button>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Input">
+                                <input type="text" class="form-control" placeholder="Output">
                             </div>
                         </div>
                     </div>
@@ -126,29 +105,12 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <!-- <label for="name">Name</label> --> <input class="form-control" type="text"
-                                    placeholder="Descripción">
-                            </div>
+                        <div class="input-group">
+                            <textarea class="form-control" aria-label="With textarea" placeholder="Descripción"></textarea>
                         </div>
                     </div>
                     <!-- Filtro seccion -->
                     <div class="d-flex flex-row">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <input type="number" id="replyNumber" min="0" data-bind="value:replyNumber"
-                                        placeholder="Ponderacion" required v-model="ponderation">
-                                </div>
-                            </div>
-                        </div> &nbsp;&nbsp;
-                        <div class="content-wrapper">
-                            <label for="datepicker">Fecha y hora</label>
-                            <datepicker v-model="selectedDate" format="yyyy-MM-dd HH:mm" :clear-button="true"
-                                :editable="false">
-                            </datepicker>
-                        </div> &nbsp;&nbsp;
                         <label for="referrer"> ¿Cual es la dificultad?
                             <select id="inputState" class="form-select" v-model="selectedDifficulty">
                                 <option selected>Choose...</option>
@@ -170,36 +132,29 @@
                         </div>
                     </div>
                     <!-- Seleccion Input seccion -->
-                    <div class="input-group">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="radio" name="option" value="option1"
-                                aria-label="Radio button 1">
-                        </div>
-                        <input type="text" class="form-control" aria-label="Text input for option 1">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                        <label class="form-check-label" for="flexSwitchCheckChecked">Habilitar pista</label>
                     </div>
-
-                    <div class="input-group">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="radio" name="option" value="option2"
-                                aria-label="Radio button 2">
+                    <div class="d-flex flex-row">
+                        <div class="row">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Respuesta correcta">
+                                <input type="text" class="form-control" placeholder="Explicación">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Opción 1">
+                                <input type="text" class="form-control" placeholder="Explicación">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Opción 2">
+                                <input type="text" class="form-control" placeholder="Explicación">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Opción 3">
+                                <input type="text" class="form-control" placeholder="Explicación">
+                            </div>
                         </div>
-                        <input type="text" class="form-control" aria-label="Text input for option 2">
-                    </div>
-
-                    <div class="input-group">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="radio" name="option" value="option3"
-                                aria-label="Radio button 3">
-                        </div>
-                        <input type="text" class="form-control" aria-label="Text input for option 3">
-                    </div>
-
-                    <div class="input-group">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="radio" name="option" value="option4"
-                                aria-label="Radio button 4">
-                        </div>
-                        <input type="text" class="form-control" aria-label="Text input for option 4">
                     </div>
                 </div>
             </div>
@@ -207,39 +162,24 @@
     </div>
 </template>
   
-<script setup>
-import { ref } from 'vue';
-import Datepicker from 'vue3-datepicker';
+<script>
+import { ref, computed } from 'vue';
 
-const questionType = ref('anz');
+export default {
+    setup() {
+        const questionType = ref('codigo');
 
-function handleQuestionTypeChange(type) {
-    questionType.value = type;
-}
+        const isCodigoQuestionType = computed(() => questionType.value === 'codigo');
+        const isMultipleQuestionType = computed(() => questionType.value === 'multiple');
 
-const selectedDate = ref('');
-const selectedTheme = ref('');
-const selectedDifficulty = ref('');
-const ponderation = ref('');
-const boxOutCode = ref([]); // Array to hold box texts
-const boxIntCode = ref([]);
+        // Remaining setup logic
 
-const addOutCode = () => {
-    boxOutCode.value.push('');
-};
-
-// Method to delete the last box text
-const deleteOutCode = () => {
-    boxOutCode.value.pop();
-};
-
-const addIntCode = () => {
-    boxIntCode.value.push('');
-};
-
-// Method to delete the last box text
-const deleteIntCode = () => {
-    boxIntCode.value.pop();
+        return {
+            questionType,
+            isCodigoQuestionType,
+            isMultipleQuestionType
+        };
+    },
 };
 </script>
 
@@ -247,7 +187,7 @@ const deleteIntCode = () => {
 <!-- Style -->
 <style >
 body {
-    background-color: #304878
+    background-image: url("http://i.imgur.com/w16HASj.png");
 }
 
 .card {
