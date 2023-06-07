41f350e131a1d0e6c914af0933d0fd0e3426b414
Santiago León <85116254+Sant1001@users.noreply.github.com>
2023-06-07 00:20:04 -0600

    SP4T9 Modulos y actividades de main listos

--- Front/src/components/AcordeonTareasMain.vue
+++ Front/src/components/AcordeonTareasMain.vue
@@ -44,28 +44,37 @@ onMounted(
 </script>
 <template>
   <div class="accordion" id="accordionExample">
-    <div class="accordion-item" v-for="(accordionItem1,i) in modulos" :key="i">
+    <div class="accordion-item" v-for="(accordionItem1, i) in modulos" :key="i">
       <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseOne' + i" aria-expanded="false" :aria-controls="'collapseOne' + i">
           {{ accordionItem1.tipo }}
         </button>
       </h2>
-      <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div :id="'collapseOne' + i" class="accordion-collapse collapse" :data-bs-parent="'#accordionExample' + i">
         <div class="accordion-body">
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item" v-for="(accordionItem,index) in data" :key="index" > 
+          <div class="accordion" :id="'accordionExample' + i">
+            <div class="accordion-item" v-for="(accordionItem, index) in data" :key="index">
               <div v-if="accordionItem1.tipo == accordionItem.tema.tipo">
                 <h2 class="accordion-header">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapseTwo' + i + index" aria-expanded="false" :aria-controls="'collapseTwo' + i + index">
                     <strong>{{ accordionItem.titulo}}</strong>
                   </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div :id="'collapseTwo' + i + index" class="accordion-collapse collapse" :data-bs-parent="'#accordionExample' + i">
                   <div class="accordion-body">
                     <strong>Profesor:</strong> {{ accordionItem.profesor.nombre + ' ' + accordionItem.profesor.apellidoPat+ ' ' + accordionItem.profesor.apellidoMat}} <br>
                     <strong>Dificultad:</strong> {{ accordionItem.dificultad.rango }} <br>
-                    <strong>Departamento:</strong> {{ accordionItem.profesor.departamento.nombre }} <br>
                     <strong>Especialidad:</strong> {{ accordionItem.profesor.especialidad }}
+                  </div>
+                </div>
+              </div>
+              <div v-else>
+                <div class="alert alert-primary d-flex align-items-center" role="alert" style="margin-bottom: 0;">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:" style="width: 5%; height: auto;">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>
+                  <div>
+                    No hay actividades en este modulo.
                   </div>
                 </div>
               </div>
@@ -75,8 +84,9 @@ onMounted(
       </div>
     </div>
   </div>
-    <RouterView />
+  <RouterView />
 </template>
+
 
 <script>
 export default {
