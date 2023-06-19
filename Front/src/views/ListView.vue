5566d96f8f54f369c1612c89b87d6c8f227ac7cd
JosuAlejandroAl <A01732454@itesm.mx>
2023-06-19 04:57:50 -0600

    Subir Actividad Listo
    cambios en la posible mejora en duracion

--- Front/src/views/ListView.vue
+++ Front/src/views/ListView.vue
@@ -1,144 +1,208 @@
 <template>
     <div>
-        <header>
-            <NavInit />
-        </header>
-
-        <body style="padding-top: 6%;">
-            <h2>{{ nombre }} elige las preguntas para su Actividad</h2>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Check</th>
-                            <th>Título</th>
-                            <th>Tema</th>
-                            <th>Dificultad</th>
-                            <th>Profesor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in items" :key="item.id">
-                            <td>
-                                <input type="checkbox" v-model="item.checked" @change="updateSelectedQuestions(item)" />
-                            </td>
-                            <td>{{ item.titulo }}</td>
-                            <td>{{ item.tema }}</td>
-                            <td>{{ item.dificultad }}</td>
-                            <td>{{ item.nombreProfesor }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div class="card">
+      <header>
+        <NavInit />
+      </header>
+  
+      <body style="padding-top: 6%;">
+        <h2>{{ nombre }} elige las preguntas para su Actividad</h2>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Check</th>
+                <th>Título</th>
+                <th>Tema</th>
+                <th>Dificultad</th>
+                <th>Profesor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.id">
+                <td>
+                  <input type="checkbox" v-model="item.checked" @change="updateSelectedQuestions(item)" />
+                </td>
+                <td>{{ item.titulo }}</td>
+                <td>{{ item.tema }}</td>
+                <td>{{ item.dificultad }}</td>
+                <td>{{ item.nombreProfesor }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+  
+        <div class="card">
+          <div class="row">
+            <div class="col-md-8 cart">
+              <div class="title">
                 <div class="row">
-                    <div class="col-md-8 cart">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col">
-                                    <h4><b>Previsualizador de Actividad</b></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row border-top border-bottom" v-for="(task, index) in selectedTasks" :key="index">
-                            <div class="row main align-items-center">
-                                <div class="col">
-                                    <div class="row">{{ task.titulo }}</div>
-                                </div>
-                                <div class="col">
-                                    <input type="number" id="typeNumber" class="form-control-sm" placeholder="0.0" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 summary">
-                        <div>
-                            <h5><b>Configuración</b></h5>
-                        </div>
-                        <hr>
-                        <form>
-                            <p>Tiempo Limite</p>
-                            <input type="date" id="date" placeholder="Select a date">
-                            <input type="time" id="time" placeholder="Select a time">
-                            <p>Titulo</p>
-                            <input placeholder="Titulo de la Actividad">
-                            <p>Grupo</p>
-                            <select id="grupo-filter" class="form-control" v-model="selectedGroupId">
-                                <option v-for="(item, i) in selectedGroup" :key="i">{{ item.nombre }}</option>
-                            </select>
-
-                        </form>
-                        <button class="btn">Subir Actividad</button>
-                    </div>
+                  <div class="col">
+                    <h4><b>Previsualizador de Actividad</b></h4>
+                  </div>
                 </div>
+              </div>
+              <div class="row border-top border-bottom" v-for="(task, index) in selectedTasks" :key="index">
+                <div class="row main align-items-center">
+                  <div class="col">
+                    <div class="row">{{ task.titulo }}</div>
+                  </div>
+                  <div class="col">
+                    <input type="number" id="typeNumber" class="form-control-sm" placeholder="0.0" v-model="task.valor" />
+                  </div>
+                </div>
+              </div>
             </div>
-        </body>
+            <div class="col-md-4 summary">
+              <div>
+                <h5><b>Configuración</b></h5>
+              </div>
+              <hr>
+              <form>
+                <p>FechaInicio</p>
+                <input type="date" id="date" v-model="fechaInicio" placeholder="Seleccione una fecha">
+                <p>FechaLimite</p>
+                <input type="date" id="date" v-model="fechaFin" placeholder="Seleccione una fecha">
+                <p>Duracion</p>
+                <input type="time" id="time" v-model="duracion" placeholder="Seleccione una hora">
+                <p>Título</p>
+                <input v-model="tituloActividad" placeholder="Título de la Actividad">
+                <p>Grupo</p>
+                <select id="grupo-filter" class="form-control" v-model="selectedGroupId">
+                  <option v-for="(item, i) in selectedGroup" :key="i" :value="item.id">{{ item.nombre }}</option>
+                </select>
+              </form>
+              <button class="btn" @click="subirActividad">Subir Actividad</button>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
-</template>
+  </template>
   
-<script setup>
-import { onMounted, ref, defineProps } from 'vue';
-import axios from 'axios';
-import NavInit from '../components/NavInitProf.vue';
-
-const items = ref([]);
-const selectedTasks = ref([]); // Tareas seleccionadas en la card
-const nombre = ref(null);
-const selectedGroup = ref(''); // Grupo seleccionado en el formulario
-
-
-onMounted(() => {
-    axios.get('http://127.0.0.1:8000/api/pregunta/')
-        .then(result => {
-            const data = result.data.map(item => ({
-                titulo: item.titulo,
-                dificultad: item.dificultad.rango,
-                tema: item.tema.tipo,
-                nombreProfesor: item.profesor.nombre,
-                checked: false, // Agregamos la propiedad "checked" a cada pregunta
-            }));
-            items.value = data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-
-onMounted(() => {
-    axios.get('http://127.0.0.1:8000/api/grupo/')
-        .then(result => {
-            console.log(result.data)
-            selectedGroup.value = result.data
-        })
-        .catch(error => {
-            console.log(error)
-        })
-})
-
-const { idUsuario } = defineProps(['idUsuario']);
-
-axios.get(`http://127.0.0.1:8000/api/profesor/${idUsuario}`)
-    .then(response => {
-        nombre.value = response.data.nombre;
+  <script setup>
+  import { onMounted, ref, defineProps } from 'vue';
+  import axios from 'axios';
+  import NavInit from '../components/NavInitProf.vue';
+  
+  const items = ref([]);
+  const selectedTasks = ref([]); // Tareas seleccionadas en la card
+  const nombre = ref(null);
+  const selectedGroup = ref(''); // Grupo seleccionado en el formulario
+  const tituloActividad = ref('');
+  const fechaInicio = ref('');
+  const fechaFin = ref('');
+  const duracion = ref('');
+  const selectedGroupId = ref(0);
+  const pregunta = ref(0);
+  const valor = ref(0);
+  
+  onMounted(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/pregunta/')
+      .then((result) => {
+        const data = result.data.map((item) => ({
+          titulo: item.titulo,
+          dificultad: item.dificultad.rango,
+          tema: item.tema.tipo,
+          nombreProfesor: item.profesor.nombre,
+          pregunta: item, // Incluye la propiedad pregunta con el objeto completo
+          checked: false,
+          valor: 0, // Nuevo valor inicial
+        }));
+        items.value = data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  
+  onMounted(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/grupo/')
+      .then((result) => {
+        console.log(result.data);
+        selectedGroup.value = result.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  
+  const { idUsuario } = defineProps(['idUsuario']);
+  
+  axios
+    .get(`http://127.0.0.1:8000/api/profesor/${idUsuario}`)
+    .then((response) => {
+      nombre.value = response.data.nombre;
     })
-    .catch(error => {
-        console.error(error);
+    .catch((error) => {
+      console.error(error);
     });
-
-function updateSelectedQuestions(item) {
+  
+  function subirActividad() {
+    // Realizar POST de la actividad
+    axios
+      .post('http://127.0.0.1:8000/api/actividad/', {
+        nombre: tituloActividad.value,
+        fechaInicio: fechaInicio.value,
+        fechaFin: fechaFin.value,
+        duracion: duracion.value,
+      })
+      .then((response) => {
+        console.log('Actividad creada:', response.data);
+  
+        // Obtener el ID de la actividad creada
+        const actividadId = response.data.id;
+        console.log(response.data.id);
+        console.log(selectedGroupId.value);
+  
+        // Realizar POST de actividadGrupo
+        axios
+          .post('http://127.0.0.1:8000/api/actividaGrupo/', {
+            grupo: selectedGroupId.value,
+            actividad: actividadId,
+          })
+          .then((response) => {
+            console.log('ActividadGrupo creada:', response.data);
+  
+            // Realizar POST de actividaPregunta para cada pregunta seleccionada
+            selectedTasks.value.forEach((task) => {
+              axios
+                .post('http://127.0.0.1:8000/api/actividaPregunta/', {
+                  actividad: actividadId,
+                  pregunta: task.pregunta.id,
+                  valor: task.valor, // Utilizar el valor correspondiente a cada pregunta
+                })
+                .catch((error) => {
+                  console.error('Error al crear actividaPregunta:', error);
+                  // Manejar el error de acuerdo a tus necesidades
+                });
+            });
+  
+            // Realizar cualquier acción adicional después de crear la actividadGrupo
+          })
+          .catch((error) => {
+            console.error('Error al crear la actividad o actividadGrupo:', error);
+            // Manejar el error de acuerdo a tus necesidades
+          });
+      })
+      .catch((error) => {
+        console.error('Error al crear la actividad:', error);
+        // Manejar el error de acuerdo a tus necesidades
+      });
+  }
+  
+  function updateSelectedQuestions(item) {
     if (item.checked) {
-        selectedTasks.value.push(item);
+      selectedTasks.value.push(item);
     } else {
-        const index = selectedTasks.value.findIndex(task => task.id === item.id);
-        if (index !== -1) {
-            selectedTasks.value.splice(index, 1);
-        }
+      const index = selectedTasks.value.findIndex((task) => task.titulo === item.titulo);
+      if (index !== -1) {
+        selectedTasks.value.splice(index, 1);
+      }
     }
-}
-</script>
-  
-  
+  }
+  </script>
 
 <style scoped>
 body {
